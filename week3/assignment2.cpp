fd49c984a187770aa7d3e29b8b0ed68df9a4d10d
Begachu <201802172@o.cnu.ac.kr>
2019-09-24 00:29:22 +0900

    201820172 한유경 한글 입력 가능하게 되어 주석을 한글로 수정함. 추가적으로 week3/assignment3.cpp 내 틀린 부분을 수정함.

--- week3/assignment2.cpp
+++ week3/assignment2.cpp
@@ -8,4 +8,5 @@ int main(){
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 }
-/*a is point constant. so we can't modify a.*/
+//a라는 포인터가 현재 가리키는 것은 상수로,
+//a가 가르키는 값을 'b'라는 임의의 값으로 변경할 수 없다.
--- week3/assignment3.cpp
+++ week3/assignment3.cpp
@@ -11,18 +11,26 @@ int main(){
 	std::cout << &name+i << std::endl;
 }
 /*
-my computer is 64 bit. so
+현재 작성한 컴퓨터는 64bit이므로 각 자료형에 할당되는 메모리는 다음과 같다.
 
-long type = 8 byte
-int type = 4 byte
-char type = 1 byte
-address type = 8 byte
+int : 4 byte
+pointer : 8 byte
+double : 8 byte
+char : 1 byte
+long : 8 byte
 
-all data in main method are stored in stack
-so i = (char name) + (char * copy) + (int * c) + (int i) + (int a)
-		+(long b) * 172
-	 = 1 + 8 + 8 + 4 + 4 + 8*172 = 1409
-but to search faster, there is blank space next to char type.
-so we have to add 3. (because search gap is 4 byte or 8 byte)
-therefore, i = 1409 + 3 = 1412.
+해당 변수들은 main()이라는 함수 내에 선언되었으므로 stack에 저장된다.
+이러한 점과 위의 자료형별 할당되는 메모리를 통해
+name 주소부터 target 주소까지의 거리를 유추해볼 수 있다.
+
+먼저 위와 같은 target의 상수를 표현하기 위해서는 최소 13bit의 메모리가 필요하다.
+이때  메모리 주소의 할당은 메모리 공간 절약보다는 탐색 속도에 초점이 맞춰져있어
+4의 배수 혹은 8의 배수로 주소를 할당하기 때문에 16bit-13bit=3bit의 빈공간이 앞에 발생한다.
+따라서 계산 시 추가로 빈공간 3bit의 값을 더해야 알맞은 결과가 나온다.
+또한 long형도 빠른 데이터 탐색 속도를 위해 16의 배수 단위로 이를 저장하기 때문에
+long형의 배열 b에도 8bit의 빈공간이 발생하게 되므로 이 또한 포함시켜야한다.
+
+i = char + const char * + int * + int + int + long*172 + 3 + 8
+  = 1 + 8 + 8 + 4 + 4 + 8*172 + 3 + 8 = 1412
+따라서 i = 1412 이다.
 */
